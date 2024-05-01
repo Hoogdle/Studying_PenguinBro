a641aac543aad39324dc263886f8e04ec1d79a5e
Taeyeong Kim <xodud7737@gmail.com>
2024-05-01 11:26:40 +0900

    Studying

--- CH7.순차적인 데이터를 처리하는 RNN/2.2. 자연어 전처리.py
+++ CH7.순차적인 데이터를 처리하는 RNN/2.2. 자연어 전처리.py
@@ -6,26 +6,32 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchtext import data, datasets
+from torchtext import data, datasets # torchtext : 자연어 데이터셋을 다루기 위한 라이브러리
 
 
+# 모델의 하이퍼파라미터 정의
 BATCH_SIZE = 64
 lr = 0.001
 EPOCHS = 40 
 USE_CUDA = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
 
+# 데이터셋을 설정
 TEXT = data.Field(sequential=True, batch_first = True, lower=True)
 LABEL = data.Field(sequential=False, batch_first = True)
 
+# trainset과 testset으로 데이터 나눠주기
 trainset,testset = datasets.IMDB.splits(TEXT,LABEL)
 
-TEXT.build_vocab(trainset,min_freq=5)
+# 단어 사전 만들기
+TEXT.build_vocab(trainset,min_freq=5) # 5번 이상 등장하는 단어만 사전에 담기, 5번 미만은 unk(Unknown) 토큰으로 대체
 LABEL.build_vocab(trainset)
 
-trainset, valset = trainset.split(split_ratio=0.8)
+trainset, valset = trainset.split(split_ratio=0.8) # 학습셋 80%, 검증셋 20%로 나누기
+# train_iter,val_iter,test_iter에 각각 배치 데이터 생성해주기
 train_iter, val_iter, test_iter = data.BucketIterator.splits((trainset,valset,testset),batch_size = BATCH_SIZE,shuffle=True,repeat=False)
 
+# 사전속 단어 갯수와 레이블 수를 변수에 저장
 vocab_size = len(TEXT.vocab)
 n_classes = 2
 
