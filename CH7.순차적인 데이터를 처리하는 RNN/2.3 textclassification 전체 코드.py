22f3ab046b0f935d47f7546f7b539bfc4ada1f99
Taeyeong Kim <xodud7737@gmail.com>
2024-05-07 14:42:11 +0900

    Studying

--- CH7.순차적인 데이터를 처리하는 RNN/2.3 textclassification 전체 코드.py
+++ CH7.순차적인 데이터를 처리하는 RNN/2.3 textclassification 전체 코드.py
@@ -1,7 +1,7 @@
-# ø¯∫ª ƒ⁄µÂ √‚√≥:
+# ÏõêÎ≥∏ ÏΩîÎìú Ï∂úÏ≤ò:
 # https://github.com/keon/3-min-pytorch/blob/master/07-%E1%84%89%E1%85%AE%E1%86%AB%E1%84%8E%E1%85%A1%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%8B%E1%85%B5%E1%86%AB_%E1%84%83%E1%85%A6%E1%84%8B%E1%85%B5%E1%84%90%E1%85%A5%E1%84%85%E1%85%B3%E1%86%AF_%E1%84%8E%E1%85%A5%E1%84%85%E1%85%B5%E1%84%92%E1%85%A1%E1%84%82%E1%85%B3%E1%86%AB_RNN/text_classification.py
 
-import os 
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -13,25 +13,27 @@ EPOCHS = 40
 USE_CUDA = torch.cuda.is_available()
 
 DEVICE = torch.device("cuda" if USE_CUDA else "cpu")
-TEXT = data.Field(sequential=True,batch_first=True,lower=True) 
-LABEL = data.Field(sequential=True,batch_first=True)
+TEXT = data.Field(sequential=True, batch_first=True, lower=True)
+LABEL = data.Field(sequential=False,batch_first=True)
 
 trainset, testset = datasets.IMDB.splits(TEXT,LABEL)
 
-TEXT.build_vocab(trainset, min_freq=5)
-LABEL.build_vocab(trainset)
+TEXT.build_vocab(trainset,min_freq=5)
+LABEL.build_vocab(testset)
 
 trainset, valset = trainset.split(split_ratio=0.8)
-train_iter, val_iter, test_iter = data.BucketIterator.splits((trainset, valset, testset), batch_sizes=BATCH_SIZE, shuffle=True, repeat=False)
+train_iter, val_iter, test_iter = data.BucketIterator.splits((trainset,valset,testset),batch_size=BATCH_SIZE,shuffle=True,repeat=False)
 
 vocab_size = len(TEXT.vocab)
 n_classes = 2
-print("[TRAIN] : %d \t [TEST] : %d \t [VOCAB] : %d \t [CLASSES] : %d" %(len(train_iter),len(test_iter),vocab_size,n_classes))
+
+print("[TRAIN] : %d\t [TEST] : %d\t [VOCAB] : %d\t [CLASSES] : %d\n"%(len(trainset),len(valset),vocab_size,n_classes))
+# [TRAIN] : 20000  [TEST] : 5000   [VOCAB] : 46159         [CLASSES] : 2
 
 class BasicGRU(nn.Module):
-    def __init__(self, n_layers, hidden_dim, n_vocab, embed_dim, n_classes, dropout_p=0.2):
+    def __init__(self,n_layers,hidden_dim,n_vocab,embed_dim,n_classes,dropout_p=0.2):
         super(BasicGRU,self).__init__()
-        print("Building Basic GRU model...")
+        print("Building Basic GRU Model")
         self.n_layers = n_layers
         self.embed = nn.Embedding(n_vocab,embed_dim)
         self.hidden_dim = hidden_dim
@@ -41,13 +43,11 @@ class BasicGRU(nn.Module):
 
     def forward(self,x):
         x = self.embed(x)
-        h_0 = self._init_state(batch_size=x.size(0))
+        h_0 = self._init_state(batch_size = x.size(0))
         x, _ = self.gru(x,h_0)
         h_t = x[:,-1,:]
         self.dropout(h_t)
         logit = self.out(h_t)
         return logit
     
-    def _init_state(self,batch_size=1):
-        weight = next(self.parameters()).data
-        return weight.new(self.n_layers,batch_size,self.hidden_dim).zero_()
+    def _init_state(self,)
